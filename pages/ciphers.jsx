ff4a5c43b15a2c655866c78318ef493dbb928bb5
lilycthor <lilycthorsteinsson@gmail.com>
2021-10-20 21:12:44 +0100

    added cipher page

---
+++ pages/ciphers.jsx
@@ -0,0 +1,7 @@
+export default function Ciphers() {
+  return (
+    <div className={`pageBody`}>
+      <h1>Ciphers</h1>
+    </div>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,7 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
+    <div className={`pageBody`}>
       <h1>Homepage</h1>
     </div>
   );
