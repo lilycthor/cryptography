1e6d114794ebcbbdd55f89cce7e984026b6377e6
lilycthor <lilycthorsteinsson@gmail.com>
2021-10-20 22:17:40 +0100

    added counter using useState

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,17 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-
+import { useState } from 'react';
 export default function Home() {
+
+  const [counter, updateCounter] = useState(0)
+
   return (
     <div className={`pageBody`}>
       <h1>Homepage</h1>
+      <button onClick={()=>updateCounter(counter-1)}>-</button>
+      <p>{counter}</p>
+      <button onClick={()=>updateCounter(counter+1)}>+</button>
     </div>
   );
 }
